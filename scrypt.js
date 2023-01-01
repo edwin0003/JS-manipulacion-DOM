c9e883467ac88efdd20f0029d7807ceaed561114
edwin0003 <edwincrespomarin@gmail.com>
2023-01-01 16:18:33 -0400

    clase04 completada

--- scrypt.js
+++ scrypt.js
@@ -1,22 +1,44 @@
-const btn = document.querySelector('[data-form-btn]')
+(() => {
 
-console.log(btn);
+    const btn = document.querySelector('[data-form-btn]')
 
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector('[data-form-input')
-    const value = input.value;
-    const list = document.querySelector('[data-list]')
-    const task = document.createElement('li')
-    task.classList.add('card');
-    input.value = "";
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content;
-    list.appendChild(task)
-    console.log(value);
-}
-btn.addEventListener("click", createTask)
+    console.log(btn);
+
+    const createTask = (evento) => {
+        evento.preventDefault();
+        const input = document.querySelector('[data-form-input')
+        const value = input.value;
+        const list = document.querySelector('[data-list]')
+        const task = document.createElement('li')
+        task.classList.add('card');
+        input.value = "";
+
+        const taskContent = document.createElement('div')
+        const titleTask = document.createElement('span')
+
+        titleTask.classList.add("task")
+        titleTask.innerText = value
+
+        taskContent.appendChild(checkComplete())
+        taskContent.appendChild(titleTask)
+
+        task.appendChild(taskContent)
+        list.appendChild(task)
+        console.log(value);
+    }
+
+    const checkComplete = () => {
+        const i = document.createElement("i")
+        i.classList.add("far", "fa-check-square", "icon")
+        i.addEventListener("click", completeTask)
+        return i;
+    }
+
+    const completeTask = (event) => {
+        const elemt = event.target;
+        elemt.classList.toggle("fas")
+        elemt.classList.toggle("completeIcon")
+        elemt.classList.toggle("far")
+    }
+    btn.addEventListener("click", createTask)
+})();
